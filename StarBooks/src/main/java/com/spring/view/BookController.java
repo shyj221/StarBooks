6129c81438fbbe32ca4cae60da47c99a03439547
ooo90 <ooo90@LAPTOP-O00BL4KF>
2020-11-28 12:40:22 +0900

    20201128_ahyeon2

--- StarBooks/src/main/java/com/spring/view/BookController.java
+++ StarBooks/src/main/java/com/spring/view/BookController.java
@@ -2,13 +2,23 @@ package com.spring.view;
 
 import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.biz.BookService;
+import com.spring.biz.vo.MenuVO;
 
 @Controller
 public class BookController {
 	@Resource(name = "bookService")
 	BookService bookService;
+	
+	@RequestMapping(value = "/loginInBook.do")
+	public String loginInBook(MenuVO menuVO) {
+		menuVO.setMenuKind("bookMenu");
+		return "book/loginInBook"; 
+	}
+	
+	
 	
 }
 
